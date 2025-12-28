8ffffbcc79887e1eaec20068e107cde20cd9caf5
VictorDeveloper2026 <victorprogramador3@gmail.com>
2025-12-28 17:17:41 -0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+<div class="p-10 bg-gray-100 min-h-screen">
+  <h1 class="text-4xl font-bold text-blue-600">
+    Tailwind funcionando 🎉
+  </h1>
+
+  <p class="mt-4 text-gray-700">
+    Agora sim está estilizado!
+  </p>
+
+  <button class="mt-6 px-4 py-2 bg-blue-500 text-white rounded">
+    Clique aqui
+  </button>
+</div>
+`;
